56acb7b046720bb2bb2cadece325a38c6b9fd72f
Rob Hyndman <robjhyndman@gmail.com>
2024-02-23 14:49:37 +1100

    More work on week 4

--- week4/examples.R
+++ week4/examples.R
@@ -14,3 +14,36 @@ traceback()
 
 options(error = recover)
 f("a")
+
+
+# Example 2
+
+transform_number <- function(x) {
+  square <- x^2
+  if (x >= 0) {
+    logx <- log(x)
+    sqrtx <- sqrt(x)
+  } else {
+    stop("x must be positive")
+  }
+  return(c(squared = square, log = logx, sqrt = sqrtx))
+}
+transform_number(2)
+transform_number(-1)
+transform_number(NA)
+transform_number("3")
+
+# Example 4
+
+# Multivariate scaling function
+mvscale <- function(object) {
+  # Remove centers
+  mat <- sweep(object, 2L, colMeans(object))
+  # Scale and rotate
+  S <- var(mat)
+  U <- chol(solve(S))
+  z <- mat %*% t(U)
+  # Return orthogonalized data
+  return(z)
+}
+mvscale(mtcars)
