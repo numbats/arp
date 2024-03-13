f00471a19c4d9269e9b82bf0d710f63f8f15038b
Rob Hyndman <robjhyndman@gmail.com>
2024-03-13 11:38:51 +1100

    Added code from week3 workshop

--- week3/doubler.R
+++ week3/doubler.R
@@ -11,35 +11,40 @@ create_from_github(
   protocol = "ssh"
 )
 # Rename folder and Rproj to your package name
-create_package("~/Desktop/doubler")
+create_package(".")
+# Add github links to DESCRIPTION file
 use_github_links()
-
+# Use GPL-3 licence
 use_gpl3_license()
-
-
+# Create first R file in R folder
 use_r("dblr")
+# Go somewhere in R file and select "Insert Roxygen Skeleton" from RStudio code menu. Then complete documentation
+# Generate help files
 document()
-build()
-install()
-library(doubler)
+# Build and load package
+build() # Or Ctrl-Shift-B
+# Load current version of package without rebuilding it
+load_all() # Or Ctrl-Shift-L
+# Check package passes tests
+check() # Or Ctrl-Shift_E
 
-dblr(5)
-dblr(3 + 2i)
-dblr("A")
-dblr(TRUE)
-
-load_all() # Or Ctrl-L
-
-check()
-
+# Generate readme file
 use_readme_rmd()
+# Build readme file
 build_readme()
-
+# Generate vignette
 usethis::use_vignette("introduction")
-
+# Use testthat facilities for unit testing
 use_testthat()
+# Create tests for dblr() function
 use_test("dblr")
-test()
+# Run tests
+test() # Or Ctrl-Shift-T
 
+# Add package to imports
 use_package()
-use_github()
+# Add pkgdown website
+use_pkgdown()
+
+# Add github action to check package
+use_github_action_check_standard()
