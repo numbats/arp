2b9c0492c0c95806b9ccd7c3d6fade394d7aaca1
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-17 14:54:37 +1100

    Week 3 slides and activities

--- week3/doubler.R
+++ week3/doubler.R
@@ -3,13 +3,6 @@ devtools::dev_sitrep()
 library(devtools)
 # Create package with one function that doubles numbers
 
-# Since github already set up, we will do that first
-
-create_from_github(
-  repo_spec = "numbats/assignment-1-package-creation-robjhyndman",
-  destdir = "~/Desktop/",
-  protocol = "ssh"
-)
 # Rename folder and Rproj to your package name
 create_package(".")
 # Add github links to DESCRIPTION file
