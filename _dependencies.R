803df7f20d1758f46921397d900623e9921e388e
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-25 10:20:08 +1100

    Built site for gh-pages

--- _dependencies.R
+++
@@ -1,5 +0,0 @@
-library(codetools)
-library(languageserver)
-library(httpgd)
-library(RcppArmadillo)
-library(yaml)
--- week7/targets_example/_targets.R
+++
@@ -1,9 +0,0 @@
-library(targets)
-source("functions.R")
-tar_option_set(packages = c("readr", "dplyr", "ggplot2"))
-list(
-  tar_target(file, "data.csv", format = "file"),
-  tar_target(data, get_data(file)),
-  tar_target(model, fit_model(data)),
-  tar_target(plot, plot_model(model, data))
-)
