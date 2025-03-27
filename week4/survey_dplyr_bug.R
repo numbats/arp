7841b5a9bfd9676a27386a80ce92b8c97fe80a90
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-03-27 19:59:32 +1100

    Fix W4 reprex example

--- week4/survey_dplyr_bug.R
+++ week4/survey_dplyr_bug.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(rainbow)
 
-survey_data <- read.csv("https://arp.numbat.space/week4/survey_data.csv", row.names = FALSE)
+survey_data <- read.csv("https://arp.numbat.space/week4/survey_data.csv")
 
 survey_data |>
   select(-RespondentID) |>
