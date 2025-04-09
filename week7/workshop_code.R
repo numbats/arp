98b95b87340bb10d8f2af8335893a1da14913728
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-09 15:40:03 +1000

    Started on week7 slides

--- week7/workshop_code.R
+++ week7/workshop_code.R
@@ -10,7 +10,6 @@ compute()
 compute(rerun = TRUE)
 compute()
 
-
 library(memoise)
 sq <- function(x) {
   print("Computing square of 'x'")
@@ -20,28 +19,3 @@ memo_sq <- memoise(sq)
 memo_sq(2)
 memo_sq(2)
 
-## Vectorisation
-
-calc_area <- function(N = 1e4) {
-  hits <- 0
-  N <- 1e3
-  for(i in seq(N)) {
-    u1 <- runif(1, 0,1)
-    u2 <- runif(1, 0,1)
-    if(u2 < u1^2)
-      hits <- hits + 1
-  }
-  hits/N
-}
-
-calc_area2 <- function(N = 1e4) {
-  u1 <- runif(N, 0, 1)
-  u2 <- runif(N, 0, 1)
-  mean(u2 < u1^2)
-}
-
-bench::mark(
-  calc_area(),
-  calc_area2(),
-  check = FALSE
-)
