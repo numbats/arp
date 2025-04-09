bf5458740db6d8a549f0a0eca08eaf0475dea084
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-09 16:10:20 +1000

    Fixed targets example to match slides

--- week7/targets_example/_targets.R
+++ week7/targets_example/_targets.R
@@ -1,8 +1,8 @@
 library(targets)
-source("functions.R")
 tar_option_set(packages = c("readr", "dplyr", "ggplot2"))
+tar_source()
 list(
-  tar_target(file, "data.csv", format = "file"),
+  tar_target(file, "data/data.csv", format = "file"),
   tar_target(data, get_data(file)),
   tar_target(model, fit_model(data)),
   tar_target(plot, plot_model(model, data))
