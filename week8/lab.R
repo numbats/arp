468d8dcd97870905c6e181f99fd23e09b7aa1a40
Rob J Hyndman <robjhyndman@gmail.com>
2026-04-23 17:55:33 +1000

    Working on wk8

--- week8/lab.R
+++ week8/lab.R
@@ -22,17 +22,22 @@ x
 
 # print.default
 
-
 y <- print(1:10)
 y
 
-print.grade <- function(x, ...){
-  letter <- if (x < 50) "N"
-  else if (x < 60) "P"
-  else if (x < 70) "C"
-  else if (x < 80) "D"
-  else "HD"
-  cat(x," [", letter, "]", sep = "")
+print.grade <- function(x, ...) {
+  letter <- if (x < 50) {
+    "N"
+  } else if (x < 60) {
+    "P"
+  } else if (x < 70) {
+    "C"
+  } else if (x < 80) {
+    "D"
+  } else {
+    "HD"
+  }
+  cat(x, " [", letter, "]", sep = "")
   invisible(x)
 }
 unclass(x)
@@ -54,65 +59,27 @@ reverse <- function(x) {
   UseMethod("reverse")
 }
 
-# reverse.character <- function(x) {
-#   stringi::stri_reverse(x)
-#   # paste(rev(strsplit(x, "")[[1]]), collapse = "")
-# }
-
-reverse("abcde")
-
-reverse.integer <- function(x) {
-  # as.integer(NextMethod())
-  # num_numbers <- floor(log10(x)) + 1
-  # 10^num_numbers - x
-  # sign_x <- sign(x)
-  # x <- abs(x)
-  # reversed <- 0
-  # while(x > 0) {
-  #   digits <- x %% 10
-  #   reversed <- reversed * 10 + digits
-  #   x <- x %/% 10
-  # }
-  # as.integer(sign_x * reversed)
-
-  ndigits <- floor(log10(x))
-  digits <- seq(floor(log10(x)))
-  x*(10^ndigits) - (99*sum(floor(x*10^-digits)*(10^(ndigits-digits))))
+reverse.character <- function(x, ...) {
+  stringi::stri_reverse(x)
 }
 
-revint_math <- function(x) {
-  ndigits <- floor(log10(x))
-  digits <- seq(floor(log10(x)))
-  x*(10^ndigits) - (99*sum(floor(x*10^-digits)*(10^(ndigits-digits))))
+reverse.integer <- function(x, ...) {
+  as.integer(reverse(as.character(x)))
 }
-revint_base10 <- function(x) {
-  sign_x <- sign(x)
-  x <- abs(x)
-  reversed <- 0
-  while(x > 0) {
-    digits <- x %% 10
-    reversed <- reversed * 10 + digits
-    x <- x %/% 10
-  }
-  sign_x * reversed
-}
-revint_stri <- function(x) {
-  stringi::stri_reverse(as.character(x))
+reverse.double <- function(x, ...) {
+  as.double(reverse(as.character(x)))
 }
 
-options(scipen = 102938)
-bench::mark(
-  revint_base10(7653187221304),
-  revint_math(7653187221304),
-  revint_stri(7653187221304),
-  max_iterations = 100000
-)
+reverse(9197)
+
+reverse("desserts")
+
+reverse(1.9599)
 
 reverse(2345678L)
 
 reverse.double <- function(x) {
   as.double(NextMethod())
-  # as.double(reverse(as.character(x)))
 }
 
 reverse.default <- function(x) {
@@ -126,48 +93,43 @@ reverse(Sys.Date())
 reverse(palmerpenguins::penguins)
 
 
-tsibble:::summarise.grouped_ts
-
-
-
-m <- lm(log(Volume)~log(Girth)+log(Height),data=trees)
+m <- lm(log(Volume) ~ log(Girth) + log(Height), data = trees)
 str(m)
-
-
-
-tibble::tibble(
-  x = 1:10,
-  y = log(x)
-)
-
-tibble::new_tibble(
-  list(
-    x = 1:10,
-    y = log(x)
-  )
-)
 
 
 fraction <- function(numerator, denominator) {
   # Validate inputs
   stopifnot(is.numeric(numerator))
   stopifnot(is.numeric(denominator))
-  if (any(denominator == 0)) stop("I won't let you divide by 0.")
+  stopifnot(length(numerator) == length(denominator))
+  if (any(denominator == 0)) {
+    stop("I won't let you divide by 0.")
+  }
 
   # Create the data structure (list)
   x <- list(numerator = numerator, denominator = denominator)
 
   # Return a classed S3 object
-  structure(x, class = "fraction")
+  structure(x, class = "fraction") |>
+    simplify_fraction()
 }
+simplify_fraction <- function(x) {
+  gcd <- DescTools::GCD(x$numerator, x$denominator)
+  x$numerator <- x$numerator / gcd
+  x$denominator <- x$denominator / gcd
+  x
+}
+
+fraction(2, 4)
+
 
 fraction(sample(1:100, 10), sample(1:100, 10))
 
 print
 print.fraction <- function(x, ...) {
-  cat(paste(x$numerator, "/", x$denominator), sep = "\n")
+  cat(paste0(x$numerator, "/", x$denominator), sep = "\n")
 }
-fraction(sample(1:100, 10), sample(1:100, 5))
+fraction(sample(1:100, 5), sample(1:100, 5))
 
 reverse
 reverse.fraction <- function(x) {
@@ -180,6 +142,7 @@ reverse.fraction <- function(x) {
 reverse(
   fraction(2, 7)
 )
+reverse(fraction(1, 7))
 
 identical(as.numeric, as.double)
 
@@ -189,28 +152,25 @@ as.double.fraction <- function(x, ...) {
 as.numeric(fraction(2, 7))
 as.double(fraction(2, 7))
 
-
-fraction(2,7) + 3
+fraction(2, 7) + 3
 
 `%add%` <- function(e1, e2) {
   # as.numeric(e1) + as.numeric(e2)
-  fraction(
-  numerator = fraction$numerator + numeric*fraction$denominator,
-  denominator = fraction$denominator
+  if (is.numeric(e1)) {
+    e1 <- fraction(numerator = e1, denominator = 1)
+  }
+  if (is.numeric(e2)) {
+    e2 <- fraction(numerator = e2, denominator = 1)
+  }
+  common_denominator <- e1$denominator * e2$denominator
+  x <- fraction(
+    numerator = e1$numerator * e2$denominator + e2$numerator * e1$denominator,
+    denominator = common_denominator
   )
+  simplify_fraction(x)
 }
-fraction(2,7) %add% 3
-3 %add% fraction(2,7)
+fraction(2, 7) %add% fraction(3, 5)
+fraction(1, 8) %add% fraction(1, 4)
 
-fraction(2,7) %add% fraction(2,7)
-
-
-S7::class_character
-
-# S3
-best_grade <- function(x = fraction(), y = numeric(), ...) {
-  UseMethod("best_grade", y)
-}
-
-# S7
-best_grade <- new_generic("best_grade", dispatch_args = c("y"))
+fraction(2, 7) %add% 3
+3 %add% fraction(2, 7)
